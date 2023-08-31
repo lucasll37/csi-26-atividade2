fc5b95494a20f869ad2ab9a0c29badecdcdf85da
Lucas <lucas.silva1037@gmail.com>
2023-08-31 20:13:44 -0300

    up

--- js/index.js
+++ js/index.js
@@ -8,6 +8,18 @@ const ctx = canvas.getContext('2d');
 let hold_x;
 let hold_y;
 
+const audioPlane = document.getElementById('plane');
+const audioBomb = document.getElementById('bomb');
+const audioVouCair = document.getElementById('vouCair');
+
+
+
+audioPlane.addEventListener('ended', () => {
+    audioPlane.play();
+});
+
+
+
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -31,19 +43,29 @@ function drawPlane() {
 }
 
 function drawMissile() {
-    delta = Math.atan((mouseX - canvas.width/2)/(canvas.height - mouseY));
+    delta = Math.atan((mouseX - hold_x)/(hold_y - mouseY));
     const angleInDegrees = -90;
-    const angleInRadians = angleInDegrees * (Math.PI / 180) + delta;
+    var angleInRadians = angleInDegrees * (Math.PI / 180) + delta;
+
+
+    if(hold_y - mouseY < 0) angleInRadians += -Math.PI;
 
     if(fire) {
-        hold_x -= speed * Math.sin(angleInRadians);
-        hold_y -= speed * Math.cos(angleInRadians);
+        if(hold_y - mouseY < 0) {
+            hold_x -= speed * Math.sin(delta);
+            hold_y += speed * Math.cos(delta);
+        }
+        else {
+            hold_x += speed * Math.sin(delta);
+            hold_y -= speed * Math.cos(delta);
+        }
         if(hold_x > canvas.width || hold_x < 0 || hold_y > canvas.height || hold_y < 0) {
             hold_x = canvas.width/2 - 75;
             hold_y = canvas.height - 70;
             fire = false;
         }
     }
+    
     ctx.save();
     ctx.translate(hold_x, hold_y);
     ctx.rotate(angleInRadians);
@@ -55,31 +77,27 @@ plane.addEventListener('load', () => drawPlane(plane));
 missile.addEventListener('load', () => drawMissile(missile));
 
 canvas.addEventListener("mousemove", function(event) {
+    mouseX = event.clientX - canvas.offsetLeft;
+    mouseY = event.clientY - canvas.offsetTop;
+});
+
+
+function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = '#ccdcf4';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    mouseX = event.clientX - canvas.offsetLeft;
-    mouseY = event.clientY - canvas.offsetTop;
+
     drawPlane(plane);
     drawMissile(missile);
+
+    requestAnimationFrame(update);
+};
+
+requestAnimationFrame(update);
+
+canvas.addEventListener("click", () => {
+    fire = true
+    audioPlane.play();
 });
-
-
-// function update(event) {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     ctx.fillStyle = '#ccdcf4';
-//     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-//     mouseX = event.clientX - canvas.offsetLeft;
-//     mouseY = event.clientY - canvas.offsetTop;
-//     drawPlane(plane);
-//     drawMissile(missile);
-
-//     requestAnimationFrame(update);
-// };
-
-// requestAnimationFrame(update);
-
-canvas.addEventListener("click", () => fire = true);
 
